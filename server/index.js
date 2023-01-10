69bd22da2866660bb218d6a083d5f6d820f9451e
louvivien <vivien.richaud@gmail.com>
2023-01-10 22:02:41 +0100

    cors

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require("cors");
+
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors())
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
