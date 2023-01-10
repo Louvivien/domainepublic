aee9695026c4d7db639078ebe5b9387647ff4801
louvivien <vivien.richaud@gmail.com>
2023-01-10 20:45:20 +0100

    add client

---
+++ client/src/App.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import AuthorList from './/components/AuthorList';
+import ReactGA from 'react-ga';
+  const TRACKING_ID = "XXXX"; 
+  ReactGA.initialize(TRACKING_ID);
+  ReactGA.pageview(window.location.pathname + window.location.search);
+
+const App = () => {
+  const [name, setName] = useState('');
+  const [authors, setAuthors] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT
+
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    // Track Submit in GA
+    ReactGA.event({
+      category: 'Submit',
+      action: 'Submitted an author'
+    });  
+    // Set isLoading to true before sending the request
+    setIsLoading(true);
+    axios.get(`${API_ENDPOINT}/api/authors?name=${name}`)
+      .then(response => {
+        // Update the response state with the server's response
+        setAuthors(response.data);
+        // Set isLoading to false
+        setIsLoading(false);
+        // Reset isLoading to false after the response has been displayed
+        setIsLoading(false);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <Container>
+      <Row className="justify-content-center mt-5">
+        <Col xs={12} md={6}>
+          <h1 className="text-center mb-3">Rechercher un auteur</h1>
+          <p className="text-center mb-5">Entrer un nom pour rechercher si son oeuvre est dans le domaine public en France</p>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group>
+              <Form.Control type="text" value={name} onChange={event => setName(event.target.value)} />
+            </Form.Group>
+            <div className="text-center"style={{ marginTop: "5px", }}>
+            <Button type="submit" variant="primary">Rechercher</Button>
+            </div>
+          </Form>
+        </Col>
+      </Row>
+        
+      {isLoading ? (
+
+              <div style={{ marginTop: "10px", }}>
+
+                <Container>
+                    <Row>
+                      <Col className="text-center">Loading...</Col>
+                    </Row>
+                </Container>
+
+              </div>
+
+              ) : (
+
+              <div>
+                {authors ? (
+                  <Container style={{ marginTop: '20px' }}>
+                    {authors.length > 0 && <AuthorList authors={authors} />}
+                  </Container>
+                ) : null}
+              </div>
+
+
+              )}
+
+
+    </Container>
+
+
+
+
+
+
+  );
+};
+
+export default App;
---
+++ client/src/components/AuthorList.js
@@ -0,0 +1,34 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import { Table, Button } from 'react-bootstrap';
+
+const AuthorList = ({ authors }) => {
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Image</th>
+          <th>Nom</th>
+          <th>Domaine publique</th>
+        </tr>
+      </thead>
+      <tbody>
+        {authors.map(author => (
+          <tr key={author.eid}>
+            <td>
+              {author.img && <img src={author.img} alt={author.name} height="50px" />}
+            </td>
+            <td>{author.title}</td>
+            <td>{author.publicDomain ? (
+                    <Button variant="success">Domaine public</Button>
+                    ) : (
+                    <Button variant="secondary">Droits d'auteur</Button>
+                    )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+};
+export default AuthorList;
