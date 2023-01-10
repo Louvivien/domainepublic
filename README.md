8c4b4d80fa1347ce4021b21ab10bba51dfbeb1c8
louvivien <vivien.richaud@gmail.com>
2023-01-10 20:14:28 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Domaine publique en France
+
+Entrer un nom pour rechercher si son oeuvre est dans le domaine public en France
+
+
+### Live Demo
+
+[Domaine publique en France](https://chat-gpt-react-app-omega.vercel.app/) 
+
+
+### Deploy
+ -  [How to deploy on vercel](https://www.youtube.com/watch?v=YYmzj5DK_5s)
+
+
+
+License
+----
+
+MIT
---
+++ server/server.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.get('/api/authors', (req, res) => {
+  const name = req.query.name;
+  axios.get(`https://data.bnf.fr/fr/api/results/person?term=${name}`)
+    .then(response => {
+      const authors = response.data;
+      console.log(`First general request:${JSON.stringify(authors)}`);
+      
+      //Variable to keep track of the current request
+      let currentIndex = 0;
+
+      const makeRequest = () => {
+        const author = authors[currentIndex];
+        if (author) {
+          if (author.title) console.log(`Making request to https://data.bnf.fr/fr/api/${author.eid}/informations for ${author.title}`);
+          axios.get(`https://data.bnf.fr/fr/api/${author.eid}/informations`)
+            .then(authorInfos => {
+              const authorInfosData = authorInfos.data
+              if (!authorInfosData) {
+                console.log("Error: Invalid response data");
+                res.sendStatus(500);
+                return;
+              }
+              //console.log("Response data:", authorInfosData);
+              //Look for Deathdate and set publicDomain info
+              if (authorInfos.data.details) {
+                const deathSection = authorInfos.data.details.find(([key]) => key === "Mort");
+                if (deathSection) {
+                  const deathDateMatch = /content="(\d{4}-\d{2}-\d{2})"/.exec(deathSection[1]);
+                  if (deathDateMatch) {
+                    const deathDate = new Date(deathDateMatch[1]);
+                    const deathYear = deathDate.getFullYear();
+                    const currentYear = new Date().getFullYear();
+                    if (deathYear && deathYear + 70 > currentYear) {
+                        author.publicDomain = false;
+                      } else {
+                        author.publicDomain = true;
+                      }                      
+                  }
+                }        
+                author.img = authorInfos.data.cartouche.img_url;
+              };
+              currentIndex++;
+              //Recursively call the function with a delay of 1 sec (if set to 1000)
+              setTimeout(makeRequest, 0);
+            })
+            .catch(error => {
+              if (author.title) console.log(`Error in request for author ${author.title} error : ${error}`);
+              currentIndex++;
+              //Recursively call the function with a delay of 1 sec (if set to 1000)
+              setTimeout(makeRequest, 0);
+            });
+        }else{
+          //If no more authors left, return the response
+          res.send(authors);
+        }
+      }
+      makeRequest();
+    })
+    .catch(error => {
+      console.log(error);
+      res.sendStatus(500);
+    });
+});
+
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
+
